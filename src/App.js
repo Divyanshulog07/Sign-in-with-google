b13d50b4df0a724e223c6f83b23df1485865877c
Divyanshu Bansod <108691991+Divyanshulog07@users.noreply.github.com>
2024-05-25 22:28:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <GoogleLogin
+          onSuccess={(credentialResponse) => {
+            const credentialResponseDecoded = jwtDecode(
+              credentialResponse.credential
+            );
+            console.log(credentialResponseDecoded);
+          }}
+          onError={() => {
+            console.log("Login Failed");
+          }}
+        />
+      </div>
+    </>
+  );
+};
+
+export default App;
+
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId="913009780873-oatpr6o2me8mpg59peba80vrjj9e3hhg.apps.googleusercontent.com">
+    <App />
+    </GoogleOAuthProvider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
